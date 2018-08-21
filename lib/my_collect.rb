cfcef485405b97e73ff1854e877fc930bc2203e4
braj011 <bhagyaraj01@gmail.com>
2018-08-21 20:17:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(words) # put argument(s) here
+  i = 0
+  new_array = []
+  while i < words.length
+    new_array << yield(words[i])
+    # words[i] is an argument to Yield, hence use ()
+    i += 1
+  end
+  new_array
+end
 
